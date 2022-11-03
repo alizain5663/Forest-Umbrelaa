ae97f0b96fbd2e99a814166653f37a9352f5bfb3
TahaShek <sohailtaha128@gmail.com>
2022-11-03 23:15:50 +0500

    home works

--- Forest-Umbrella/src/app/main-module/main-module.module.ts
+++ Forest-Umbrella/src/app/main-module/main-module.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MainModuleRoutingModule } from './main-module-routing.module';
 import { MainModuleComponent } from './main-module.component';
 import { HeaderComponent } from './MainComponent/header/header.component';
+import { HomeComponent } from './MainComponent/home/home.component';
+import { FooterComponent } from './MainComponent/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     MainModuleComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
