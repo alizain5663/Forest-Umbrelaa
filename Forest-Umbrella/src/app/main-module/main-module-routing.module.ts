cf4bf1b772e5f3cd7c1161d1a7766f6dc2110aa0
alizain5663 <malikzain013@gmail.com>
2022-11-03 23:03:19 +0500

    add

--- Forest-Umbrella/src/app/main-module/main-module-routing.module.ts
+++ Forest-Umbrella/src/app/main-module/main-module-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainModuleComponent } from './main-module.component';
 
-const routes: Routes = [{ path: '', component: MainModuleComponent }];
+const routes: Routes = [{ path: '', component: MainModuleComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
